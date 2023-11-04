ba29035694ce2cbcf294fe26d29bb935bdc626b8
UR Raju <riadhasanraju>
2023-11-05 01:15:22 +0600

    seconds commit get all data completed

--- index.js
+++ index.js
@@ -24,14 +24,17 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-   
     await client.connect();
+    const roomsCollection = client.db('hotelDB').collection('rooms')
+
+    app.get('/rooms', async(req,res) => {
+        const result = await roomsCollection.find().toArray()
+        res.send(result)
+    })
  
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
- 
-    await client.close();
   }
 }
 run().catch(console.dir);
