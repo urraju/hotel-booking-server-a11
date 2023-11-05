812035f484c78f8ff130c1a4d76867561b429e71
UR Raju <riadhasanraju>
2023-11-05 18:26:07 +0600

    some work token related

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 require('dotenv').config()
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const app = express();
 const port = process.env.PORT || 3000;
 
 
-app.use(cors())
+app.use(cors({
+  origin : ['http://localhost:5173']
+}))
 app.use(express.json())
 
-
+app.use(cookieParser())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zrkwx23.mongodb.net/?retryWrites=true&w=majority`;
@@ -28,11 +32,26 @@ async function run() {
     const roomsCollection = client.db('hotelDB').collection('rooms')
     const myCollection = client.db('hotelDB').collection('mybooking')
     // My Booking Now part 
+    app.post('/mybooking', async(req,res) => {
+        const query = req.body
+        const result = await myCollection.insertOne(query)
+        res.send(result)
+    })
     app.get('/mybooking', async(req,res) => {
-        const result = await myCollection.find().toArray()
+      console.log(req.query.email);
+      console.log('tooken owner info', req.user);
+      if(req.user.email !== req.user.email){
+        return res.status(403).send({message : 'Foorbiden'})
+      }
+      let query = {}
+      if(req.query?.email){
+        query = {email : req.query.email}
+      }
+        const result = await myCollection.find(query).toArray()
         res.send(result)
     })
 
+    // all rooms data part 
     app.get('/rooms', async(req,res) => {
         const result = await roomsCollection.find().toArray()
         res.send(result)
