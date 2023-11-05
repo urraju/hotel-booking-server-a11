130909c155b7550c75c47285d1266a84677cf81e
UR Raju <riadhasanraju>
2023-11-05 18:36:00 +0600

    secret and token more some work ok

--- index.js
+++ index.js
@@ -9,11 +9,32 @@ const port = process.env.PORT || 3000;
 
 
 app.use(cors({
-  origin : ['http://localhost:5173']
+  origin : ['http://localhost:5173'],
+  credentials : true
 }))
 app.use(express.json())
-
 app.use(cookieParser())
+
+// token genarate part 
+const logger = (req,res, next) => {
+  console.log(req.method, req,url);
+  next()
+}
+// verify token part 
+const verifyToken = (req,res, next) => {
+  const token = req.cookies.token
+  console.log('token in the middle wayer', token);
+  if(!token) {
+    return res.status(401).send({message : 'UnAuthorized access'})
+  }
+  jwt.verify(token, process.env.SECRET, (err, decoded) => {
+    if(err){
+      return res.status(401).send({message : 'Unauthorizes Access'})
+    }
+    req.user = decoded
+    next()
+  })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zrkwx23.mongodb.net/?retryWrites=true&w=majority`;
